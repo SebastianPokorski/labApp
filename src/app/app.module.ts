a7a588c9b010c587f74be8c885d8322c50b8cb93
unknown <s.pokorski@wp.pl>
2018-01-27 18:04:09 +0100

    up to date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SingUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
